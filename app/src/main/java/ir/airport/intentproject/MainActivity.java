a91d2c7d0b7eccb414c7d90cad97996cd6f7a219
Alisalehivalo <55212861+Alisalehivalo@users.noreply.github.com>
2019-10-26 17:54:08 -0700

    version 1.3

--- app/src/main/java/ir/airport/intentproject/MainActivity.java
+++ app/src/main/java/ir/airport/intentproject/MainActivity.java
@@ -1,12 +1,14 @@
 package ir.airport.intentproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
         Button btnProf = findViewById(R.id.btnprofile);
         Button btnDial = findViewById(R.id.btndial);
         Button btnSms = findViewById(R.id.btnSms);
+        Button btnMenu = findViewById(R.id.btnMenu);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawerlayout = findViewById(R.id.drawerLayout);
+                drawerlayout.openDrawer(GravityCompat.START);
+
+            }
+        });
         btnProf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
